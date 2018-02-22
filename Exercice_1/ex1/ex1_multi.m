74f7a174d997ba963aebd346afa256d1e2f4fdab
Mathieu DE VITI <mathieu@MacBook-Pro-de-matthieu.local>
2018-02-22 14:02:06 +0100

    Final commit for exercice 1

--- Exercice_1/ex1/ex1_multi.m
+++ Exercice_1/ex1/ex1_multi.m
@@ -83,7 +83,7 @@ fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
 alpha = 0.01;
-num_iters = 400;
+num_iters = 40000;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -91,6 +91,7 @@ theta = zeros(3, 1);
 
 % Plot the convergence graph
 figure;
+hold on;
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
@@ -104,7 +105,10 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+x_test = [1650 ; 3];
+[x_test mu sigma] = featureNormalize(x_test);
+x_test = [ones(1, 1) ; x_test];
+price = theta'*x_test; % You should change this
 
 
 % ============================================================
@@ -149,7 +153,9 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+x_test = [1650 ; 3];
+x_test = [ones(1, 1) ; x_test];
+price = theta'*x_test; % You should change this
 
 
 % ============================================================
